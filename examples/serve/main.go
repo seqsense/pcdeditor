5a524adee2000aacb1d7a33ebb7d12621f38e4f7
Atsushi Watanabe <atsushi.w@ieee.org>
2021-03-05 10:33:24 +0900

    Disable browser cache on local test

--- examples/serve/main.go
+++ examples/serve/main.go
@@ -9,8 +9,17 @@ import (
 
 func main() {
 	http.Handle("/upload/", &uploader{})
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/", &noCache{Handler: http.FileServer(http.Dir("."))})
 	http.ListenAndServe(":8080", nil)
+}
+
+type noCache struct {
+	http.Handler
+}
+
+func (h *noCache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "no-cache")
+	h.Handler.ServeHTTP(w, r)
 }
 
 type uploader struct{}
