9df54c843f826ed098faff95fd4183789bfa068b
Atsushi Watanabe <atsushi.w@ieee.org>
2021-10-11 20:58:55 +0900

    Fix .d.ts type definitions (#64)

--- pcdeditor.d.ts
+++ pcdeditor.d.ts
@@ -11,6 +11,7 @@ declare class PCDEditor {
   attach(): Promise<null>
   appendDefaultMenuboxTo(selector: string): void
   loadPCD(path: string): Promise<null>
+  loadSubPCD(path: string): Promise<null>
   load2D(yamlPath: string, imgPath: string): Promise<null>
 
   logger(any): void
@@ -18,9 +19,12 @@ declare class PCDEditor {
   private wrapId: (q: string) => string
 
   pcdeditor: {
+    importPCD(a: Blob): Promise<string>
+    importSubPCD(a: Blob): Promise<string>
+    import2D(a, b: Blob): Promise<string>
     exportPCD(): Promise<Blob>
-    loadPCD(a: string): Promise<null>
-    load2D(a, b: string): Promise<null>
+    command(cmd: string): Promise<number[][]>
+    show2D(show: boolean): Promise<string>
     exit(): void
   }
 
