545bda03f3ec35ec6e107897c32fb1deff3a6e47
f-fl0 <florent@seqsense.com>
2022-08-04 16:09:00 +0900

    Add back missing buttons

--- pcdeditor.js
+++ pcdeditor.js
@@ -793,6 +793,22 @@ class PCDEditor {
     </div>
     <hr/>
     <div class="${id('foldMenuElem')}">
+      <button id="${id('delete')}">Delete</button>
+    </div>
+    <hr/>
+    <div class="${id('foldMenuElem')}">
+      <label
+        class="${id('inputLabelShort')}"
+      >Insert pcd</label>
+      <input
+        id="${id('insertSubPcdFile')}"
+        type="file"
+        accept=".pcd"
+        style="display: none;"
+      />
+      <button id="${id('insertSubPcd')}">Select file</button>
+    </div>
+    <div class="${id('foldMenuElem')}">
       <label class="${id('inputLabel')}">Clipboard</label>
       <button id="${id('clipboardCopy')}">Copy</button>
       <button id="${id('clipboardPaste')}">Paste</button>
